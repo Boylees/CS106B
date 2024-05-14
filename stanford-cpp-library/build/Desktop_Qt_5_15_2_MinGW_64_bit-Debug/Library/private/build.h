5064a889736cb921a7d9ac8ef021a2ff78d2279e
boylees <1518409520@qq.com>
2024-05-14 14:43:25 +0800

    assignment

---
+++ stanford-cpp-library/build/Desktop_Qt_5_15_2_MinGW_64_bit-Debug/Library/private/build.h
@@ -0,0 +1,9 @@
+#ifndef SPL_BUILD_H
+#define SPL_BUILD_H
+
+#define SPL_VERSION "2020.1"
+#define SPL_INSTALL_LOCATION ""
+#define SPL_BUILD_DATE "Tue May 7 22:33:29 2024"
+#define SPL_BUILD_USER ""
+
+#endif
